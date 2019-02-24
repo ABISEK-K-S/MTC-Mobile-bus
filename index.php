16c822ae91ef9b4656f537c6417c656b17dfd513
ABISEK-K-S <39756233+ABISEK-K-S@users.noreply.github.com>
2019-02-24 17:50:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MTC Bus | index</title>
+
+	<?php include 'links/header.php'; ?>
+</head>
+<body>
+
+	<?php include 'links/nav.php'; ?>
+
+<div class="jumbotron" style="background-color:#5B5A59;">
+  <h1 class="btn btn-primary btn-lg" style="color: black;font-family: 'Ubuntu', sans-serif;">About us</h1><br>
+  <p style="color:white">
+  In 1947, the Government of Madras nationalized the passenger transport for the first time by introducing 30 buses in Madras City, side by side with the buses run by the private operators at that time. The operation was under the control of the then Madras State Transport Department. In 1972, the departmental setup was transformed into a company setup in order to inoculate a commercial approach without sacrificing the social responsibilities. Thus, PALLAVAN TRANSPORT CORPORATION LIMITED was formed under the Companies Act 1956, by the Government of Tamil Nadu on 01.01.1972 with a fleet strength of 1029 buses.</p>
+  <hr class="my-4">
+  <p style="color:white">
+    The fleet strength gradually increased correspondingly to the requirements of the public and reached 2332 on 01.01.1994. Following that Pallavan Transport Corporation Ltd was bifurcated as Dr.Ambedkhar Transport Corporation Limited and Pallavan Transport Corporation Limited on 19.01.1994. The South of the Chennai Metropolitan city from EVR Periyar road came under the operational jurisdiction of the Pallavan Transport Corporation Limited. The North of Chennai Metropolitan city from EVR Periyar Road (including EVR Periyar road) came under the operational jurisdiction of the Dr.Ambedkar Transport Corporation Limited.</p>
+
+<hr class="my-4">
+
+ <p style="color:white">In the public interest, both the divisions were amalgamated into a single corporation under the name METROPOLITAN TRANSPORT CORPORATION (CHENNAI) LTD. with effect from 10-01-2001.</p>
+
+
+</body>
+</html>
